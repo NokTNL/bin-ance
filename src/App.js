5de8239b56a73fac078a618da1050a28ea81ef2c
NokTNL <noklamtsz@gmail.com>
2022-02-04 22:44:26 +0000

    Show "loading" until prices start streaming
    Added the "isLoadingPrice" state to hide the exchange rate div until
    price comes in

--- src/App.js
+++ src/App.js
@@ -17,16 +17,23 @@ function App() {
     fiat: "",
   });
   const [price, setPrice] = useState("");
+  const [isPriceLoading, setIsPriceLoading] = useState(true);
   const [buyOrSell, setBuyOrSell] = useState("buy");
   const buySellArr =
     buyOrSell === "buy" ? ["fiat", "crypto"] : ["crypto", "fiat"];
 
   useEffect(() => {
+    // A function that 1. detects new prices starts coming in AND 2. sets the new price
+    const smartSetPriceFunc = (price) => {
+      setIsPriceLoading(false);
+      setPrice(price);
+    };
     // Pass the setPrice function to let the module update the price for us
-    fetchPrice.init(setPrice);
+    fetchPrice.init(smartSetPriceFunc);
   }, []);
   useEffect(() => {
     // Send fetchPrice a new pair when currencyPair changes
+    setIsPriceLoading(true);
     fetchPrice.setPair(currencyPair);
   }, [currencyPair]);
 
@@ -34,7 +41,11 @@ function App() {
     <div className="App">
       <Header />
       <BuySellPanel buyOrSell={buyOrSell} setBuyOrSell={setBuyOrSell} />
-      <ExchangeRate price={price} currencyPair={currencyPair} />
+      <ExchangeRate
+        price={price}
+        currencyPair={currencyPair}
+        isPriceLoading={isPriceLoading}
+      />
       {buySellArr.map((cat) => (
         <CurrencyInput
           currencyCat={cat}
--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function ExchangeRate({
   price,
   currencyPair: { crypto, fiat },
+  isPriceLoading,
 }) {
   const [isCryptoBase, setIsCryptoBase] = useState(true);
 
@@ -10,16 +11,20 @@ export default function ExchangeRate({
     setIsCryptoBase((prev) => !prev);
   };
 
-  return (
-    <div>
-      <span>
-        {isCryptoBase
-          ? `1 ${crypto} ≈ ${price} ${fiat}`
-          : `1 ${fiat} ≈ ${1 / price} ${crypto}`}
-      </span>
-      <button onClick={handleSwap}>
-        <img alt="Swap" />
-      </button>
-    </div>
-  );
+  if (isPriceLoading) {
+    return <div>loading...</div>;
+  } else {
+    return (
+      <div>
+        <span>
+          {isCryptoBase
+            ? `1 ${crypto} ≈ ${price} ${fiat}`
+            : `1 ${fiat} ≈ ${1 / price} ${crypto}`}
+        </span>
+        <button onClick={handleSwap}>
+          <img alt="Swap" />
+        </button>
+      </div>
+    );
+  }
 }
